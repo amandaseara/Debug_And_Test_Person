cb6d0ded13f766f4b523b54f45be592953fc66dd
amandaseara <amanda.seara@correo.ucu.edu.uy>
2022-04-21 02:14:16 -0300

    Ultimos cambios

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -13,9 +13,40 @@ namespace Tests
         }
 
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        public void TestTodoBien()
         {
-            // Insertá tu código  de pruebaaquí
+            Person julia = new Person("Julia Rodriguez", "9.999.999-9");
+            string res = $"Soy {julia.Name} y mi cédula es {julia.ID}";
+
+            Assert.AreEqual("Soy Julia Rodriguez y mi cédula es 9.999.999-9",res);
         }
+
+        [Test]
+        public void TestNombreMal()
+        {
+            Person julia = new Person("", "9.999.999-9");
+            string res = $"Soy {julia.Name} y mi cédula es {julia.ID}";
+
+            Assert.AreEqual("Soy  y mi cédula es 9.999.999-9",res);
+        }
+
+        [Test]
+        public void TestIdMal()
+        {
+            Person julia = new Person("Julia Rodriguez", "1.234.567-8");
+            string res = $"Soy {julia.Name} y mi cédula es {julia.ID}";
+
+            Assert.AreEqual("Soy Julia Rodriguez y mi cédula es ",res);
+        }
+
+        [Test]
+        public void TestTodoMal()
+        {
+            Person julia = new Person("", "1.234.567-8");
+            string res = $"Soy {julia.Name} y mi cédula es {julia.ID}";
+
+            Assert.AreEqual("Soy  y mi cédula es ",res);
+        }
+
     }
 }
